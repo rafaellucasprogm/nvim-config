960494d34731a4366ec4253c39a8b048ecaecad2
Rafael Lucas <rafael.lucasprogm@gmail.com>
2025-02-04 15:13:06 -0300

    Updated catppuccin colorscheme call function

--- init.lua
+++ init.lua
@@ -878,12 +878,15 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'catppuccin/nvim',
-    -- priority = 1000, -- Make sure to load this before all the other start plugins.
+    priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
-      vim.cmd 'colorscheme catppuccin-mocha'
+      vim.cmd.colorscheme 'catppuccin-mocha'
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
     end,
   },
 
